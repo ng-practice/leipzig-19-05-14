1545a5cd85af1a138de4aefaf696a49e969b7256
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-05-14 13:40:43 +0200

    ref: handle subscriptions

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,32 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Todo } from './models';
 import { TodosService } from './lib/todos.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit, OnDestroy {
+  sink = new Subscription();
   todos: Todo[];
 
-  constructor(private todosService: TodosService) {
-    this.todosService.getAll().subscribe(todos => (this.todos = todos));
+  constructor(private todosService: TodosService) {}
+
+  ngOnInit(): void {
+    this.sink.add(
+      this.todosService.getAll().subscribe(todos => (this.todos = todos))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.sink.unsubscribe();
   }
 
   show(todo: Todo): void {
-    this.todosService.complete(todo).subscribe(() => this.getAll());
+    this.sink.add(
+      this.todosService.complete(todo).subscribe(() => this.getAll())
+    );
   }
 
   createTodo(todo: Todo): void {
-    this.todosService.create(todo).subscribe(() => this.getAll());
+    this.sink.add(
+      this.todosService.create(todo).subscribe(() => this.getAll())
+    );
   }
 
   deleteTodo(todo: Todo): void {
-    this.todosService.delete(todo).subscribe(() => this.getAll());
+    this.sink.add(
+      this.todosService.delete(todo).subscribe(() => this.getAll())
+    );
   }
 
   getAll(): void {
-    this.todosService.getAll().subscribe(todos => (this.todos = todos));
+    this.sink.add(
+      this.todosService.getAll().subscribe(todos => (this.todos = todos))
+    );
   }
 }
